a9eea6871f5bbb1ed60878be912d5638f027a7da
torik190 <torik190@interia.pl>
2022-11-25 19:38:42 +0100

    Guzik "Pokaż więcej"
    Guzik "Pokaż więcej".
    Losowe zdjęcia na start.

--- projekt/script.js
+++ projekt/script.js
@@ -1,11 +1,35 @@
 var container;
 var images;
+var query;
+var perPage
+var page;
+var fun;
+
+start();
+function start() {
+    perPage = 10;
+    page = 1;
+    fun = ()=>{randomImages()};
+    //fun();
+}
+
+function handleFind() {
+    let q = document.getElementById("query").value.trim();
+    if(q == "")
+        return;
+    query = q;
+    page = 1;
+    fun = ()=>{findImages()};
+    fun();
+}
+
+function showMore() {
+    page += 1;
+    fun();
+}
 
 function findImages() {
-    let query = document.getElementById("query").value;
-    let perPage = 14;
-    
-    fetch("https://api.unsplash.com/search/photos?client_id=EFJGPfM_Shem4FlBoyJucYIdGNa3cj6tBZb8_sui1cM&query=" + query + "&per_page=" + perPage)
+    fetch("https://api.unsplash.com/search/photos?client_id=EFJGPfM_Shem4FlBoyJucYIdGNa3cj6tBZb8_sui1cM&query=" + query + "&per_page=" + perPage + "&page=" + page)
     .then(response => response.json())
     .then(data => data.results)
     .then(results => {
@@ -15,9 +39,20 @@ function findImages() {
     .catch((error) => {console.error(error);});
 }
 
+function randomImages() {
+    fetch("https://api.unsplash.com/photos/random?client_id=EFJGPfM_Shem4FlBoyJucYIdGNa3cj6tBZb8_sui1cM&count=" + perPage)
+    .then(response => response.json())
+    .then(results => {
+        images = results;
+        showImages();
+    })
+    .catch((error) => {console.error(error);});
+}
+
 function showImages() {
     container = document.getElementById("container");
-    container.innerHTML = "";
+    if(page == 1)
+        container.innerHTML = "";
 
     for(let id in images) {
         createImage(id);
@@ -31,8 +66,10 @@ function createImage(id) {
     container.appendChild(tile);
     tile.className = "tile";
     tile.id = id;
-    //tile.onclick = "zoomImage(this.id)";
-    tile.title = data.alt_description + "\n\nby " + data.user.name;
+    tile.onclick = () => {zoomImage(id)};
+    tile.title = "by " + data.user.name
+    if(data.description != null)
+        tile.title += "\n\n" + data.description;
 
     let image = document.createElement("img");
     tile.appendChild(image);
@@ -40,9 +77,9 @@ function createImage(id) {
     image.src = data.urls.raw + "&fm=jpg&w=400&h=400&fit=crop";
     image.alt = "alt";
 
-    tile.innerHTML += "&#9825; " + data.likes;
+    tile.innerHTML += "&#9825;&#10084; " + data.likes;
 }
 
-/*function zoomImage(id) {
+function zoomImage(id) {
     console.log(id);
-}*/
+}
